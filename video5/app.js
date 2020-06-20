cf4bf7b21a796d644499abd6c17d6af8929e3dc6
Carlton Joseph <carlton.joseph@gmail.com>
2020-06-19 18:03:26 -0700

    add express server code

---
+++ video5/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { response } = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/about-me", (req, res) => {
+  res.redirect("./about");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "localhost", () => {
+  console.log(`Listening on port ${PORT}.`);
+});
