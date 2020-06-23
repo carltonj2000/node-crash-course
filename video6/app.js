3965987bf9d5845362f33e4a04bbbcd2044ff4f6
Carlton Joseph <carlton.joseph@gmail.com>
2020-06-23 11:03:10 -0700

    add static middleware

--- video6/app.js
+++ video6/app.js
@@ -1,6 +1,10 @@
-const app = require("express")();
+const express = require("express");
+const morgan = require("morgan");
 
+const app = express();
 app.set("view engine", "ejs");
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const blogs = [
