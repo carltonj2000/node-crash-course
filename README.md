192ea3d1d2dc8f0a6f20bc8a15852a0a1b13fe95
Carlton Joseph <carlton.joseph@gmail.com>
2020-06-26 00:33:34 -0700

    add mongoose

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 The code is based on the
 [Node.js Crash Course](https://www.youtube.com/watch?v=OIBIXYLJjsI)
 video set.
+
+## Mongo
+
+### Mongoose
+
+### Mongo Docker
+
+### Mongo Atlas
+
+Download mongo shell from
+[here](https://www.mongodb.com/try/download/shell)
+, place it in your path and connect to `Cluster0` db via the command below.
+
+```bash
+mongo "mongodb+srv://cluster0-4ncwc.mongodb.net/<dbname>" --username carltonj2000
+mongosh "mongodb+srv://cluster0-4ncwc.mongodb.net/node-tuts" --username carltonj2000
+```
--- video6/app.js
+++ video6/app.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const { request } = require("express");
+
+const Blog = require("./models/blog.js");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
+/* Mongoose test
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new Blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/all-blog", (req, res) => {
+  Blog.find()
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("5ef57898c58ecb6f56f18388")
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => console.log(err));
+});
+*/
+
 app.get("/", (req, res) => {
+  return res.redirect("/blogs");
+  // old test data below
   const blogs = [
     {
       title: "Yoshi finds eggs",
@@ -28,6 +66,14 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((blogs) => {
+      res.render("index", { title: "Blog CJ", blogs });
+    })
+    .catch((err) => console.log(err));
+});
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
@@ -38,4 +84,12 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "localhost", () => console.log(`Listening on port ${PORT}.`));
+const dbURI = "mongodb://localhost/node-tuts";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, "localhost", () =>
+      console.log(`Listening on port ${PORT}.`)
+    )
+  )
+  .catch((err) => console.error(err));
